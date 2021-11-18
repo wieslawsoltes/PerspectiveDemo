aafd0beae93982712d4ee4a589ccf5e31ce64f9a
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-11-18 15:30:32 +0100

    Refactor

--- PerspectiveDemo/AdornerCanvas.axaml.cs
+++ PerspectiveDemo/AdornerCanvas.axaml.cs
@@ -8,73 +8,122 @@ namespace PerspectiveDemo
 {
     public class AdornerCanvas : TemplatedControl
     {
-        private Thumb _t;
-        private Thumb _b;
-        private Thumb _l;
-        private Thumb _r;
-        private Thumb _ul;
-        private Thumb _ur;
-        private Thumb _ll;
-        private Thumb _lr;
-        private Thumb _drag;
+        public static readonly StyledProperty<Control?> ControlProperty = 
+            AvaloniaProperty.Register<AdornerCanvas, Control?>(nameof(Control));
 
-        public static readonly StyledProperty<Canvas> CanvasProperty = 
-            AvaloniaProperty.Register<AdornerCanvas, Canvas>(nameof(Canvas));
+        public static readonly StyledProperty<Canvas?> CanvasProperty = 
+            AvaloniaProperty.Register<AdornerCanvas?, Canvas>(nameof(Canvas));
 
-        public static readonly StyledProperty<Control> ControlProperty = 
-            AvaloniaProperty.Register<AdornerCanvas, Control>(nameof(Control));
+        private Canvas? _canvas;
+        private Thumb? _drag;
+        private Thumb? _top;
+        private Thumb? _bottom;
+        private Thumb? _left;
+        private Thumb? _right;
+        private Thumb? _topLeft;
+        private Thumb? _topRight;
+        private Thumb? _bottomLeft;
+        private Thumb? _bottomRight;
 
-        public Canvas Canvas
-        {
-            get => GetValue(CanvasProperty);
-            set => SetValue(CanvasProperty, value);
-        }
-
-        public Control Control
+        public Control? Control
         {
             get => GetValue(ControlProperty);
             set => SetValue(ControlProperty, value);
+        }
+
+        public Canvas? Canvas
+        {
+            get => GetValue(CanvasProperty);
+            set => SetValue(CanvasProperty, value);
         }
 
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
         {
             base.OnApplyTemplate(e);
 
-            _t = e.NameScope.Find<Thumb>("PART_T");
-            _b = e.NameScope.Find<Thumb>("PART_B");
-            _l = e.NameScope.Find<Thumb>("PART_L");
-            _r = e.NameScope.Find<Thumb>("PART_R");
-            _ul = e.NameScope.Find<Thumb>("PART_UL");
-            _ur = e.NameScope.Find<Thumb>("PART_UR");
-            _ll = e.NameScope.Find<Thumb>("PART_LL");
-            _lr = e.NameScope.Find<Thumb>("PART_LR");
-            _drag = e.NameScope.Find<Thumb>("PART_DRAG");
+            _canvas = e.NameScope.Find<Canvas>("PART_Canvas");
+            _drag = e.NameScope.Find<Thumb>("PART_Drag");
+            _top = e.NameScope.Find<Thumb>("PART_Top");
+            _bottom = e.NameScope.Find<Thumb>("PART_Bottom");
+            _left = e.NameScope.Find<Thumb>("PART_Left");
+            _right = e.NameScope.Find<Thumb>("PART_Right");
+            _topLeft = e.NameScope.Find<Thumb>("PART_TopLeft");
+            _topRight = e.NameScope.Find<Thumb>("PART_TopRight");
+            _bottomLeft = e.NameScope.Find<Thumb>("PART_BottomLeft");
+            _bottomRight = e.NameScope.Find<Thumb>("PART_BottomRight");
 
-            _t.DragDelta += OnDragDeltaT;
-            _b.DragDelta += OnDragDeltaB;
-            _l.DragDelta += OnDragDeltaL;
-            _r.DragDelta += OnDragDeltaR;
-            _ul.DragDelta += OnDragDeltaUL;
-            _ur.DragDelta += OnDragDeltaUR;
-            _ll.DragDelta += OnDragDeltaLL;
-            _lr.DragDelta += OnDragDeltaLR;
-            _drag.DragDelta += OnDragDeltaDRAG;
+            if (_top is { })
+            {
+                _top.DragDelta += OnDragDeltaTop;
+            }
+
+            if (_bottom is { })
+            {
+                _bottom.DragDelta += OnDragDeltaBottom;
+            }
+
+            if (_left is { })
+            {
+                _left.DragDelta += OnDragDeltaLeft;
+            }
+
+            if (_right is { })
+            {
+                _right.DragDelta += OnDragDeltaRight;
+            }
+
+            if (_topLeft is { })
+            {
+                _topLeft.DragDelta += OnDragDeltaTopLeft;
+            }
+
+            if (_topRight is { })
+            {
+                _topRight.DragDelta += OnDragDeltaTopRight;
+            }
+
+            if (_bottomLeft is { })
+            {
+                _bottomLeft.DragDelta += OnDragDeltaBottomLeft;
+            }
+ 
+            if (_bottomRight is { })
+            {
+                _bottomRight.DragDelta += OnDragDeltaBottomRight;
+            }
+
+            if (_drag is { })
+            {
+                _drag.DragDelta += OnDragDeltaDrag;
+            }
+
+            if (Control is { })
+            {
+                var rect = new Rect(Canvas.GetLeft(Control), Canvas.GetTop(Control), Control.Width, Control.Height);
+
+                UpdateThumbs(rect);
+                UpdateDrag(rect);
+            }
+
+            if (Canvas is { } && _canvas is { })
+            {
+                _canvas.Width = Canvas.Width;
+                _canvas.Height = Canvas.Height;
+            }
         }
 
         private Rect GetRect()
         {
-            var ptUL = new Point(Canvas.GetLeft(_ul), Canvas.GetTop(_ul));
-            var ptUR = new Point(Canvas.GetLeft(_ur), Canvas.GetTop(_ur));
-            var ptLL = new Point(Canvas.GetLeft(_ll), Canvas.GetTop(_ll));
-            var ptLR = new Point(Canvas.GetLeft(_lr), Canvas.GetTop(_lr));
-
-            var left = Math.Min(Math.Min(ptUL.X, ptUR.X), Math.Min(ptLL.X, ptLR.X));
-            var top = Math.Min(Math.Min(ptUL.Y, ptUR.Y), Math.Min(ptLL.Y, ptLR.Y));
-            var right = Math.Max(Math.Max(ptUL.X, ptUR.X), Math.Max(ptLL.X, ptLR.X));
-            var bottom = Math.Max(Math.Max(ptUL.Y, ptUR.Y), Math.Max(ptLL.Y, ptLR.Y));
+            var topLeft = new Point(Canvas.GetLeft(_topLeft), Canvas.GetTop(_topLeft));
+            var topRight = new Point(Canvas.GetLeft(_topRight), Canvas.GetTop(_topRight));
+            var bottomLeft = new Point(Canvas.GetLeft(_bottomLeft), Canvas.GetTop(_bottomLeft));
+            var bottomRight = new Point(Canvas.GetLeft(_bottomRight), Canvas.GetTop(_bottomRight));
+            var left = Math.Min(Math.Min(topLeft.X, topRight.X), Math.Min(bottomLeft.X, bottomRight.X));
+            var top = Math.Min(Math.Min(topLeft.Y, topRight.Y), Math.Min(bottomLeft.Y, bottomRight.Y));
+            var right = Math.Max(Math.Max(topLeft.X, topRight.X), Math.Max(bottomLeft.X, bottomRight.X));
+            var bottom = Math.Max(Math.Max(topLeft.Y, topRight.Y), Math.Max(bottomLeft.Y, bottomRight.Y));
             var width = Math.Abs(right - left);
             var height = Math.Abs(bottom - top);
-
             return new Rect(left, top, width, height);
         }
 
@@ -89,109 +138,257 @@ namespace PerspectiveDemo
 
         private void UpdateThumbs(Rect rect)
         {
-            Canvas.SetLeft(_t, rect.Left + rect.Width / 2);
-            Canvas.SetTop(_t, rect.Top);
+            Canvas.SetLeft(_top, rect.Center.X);
+            Canvas.SetTop(_top, rect.Top);
 
-            Canvas.SetLeft(_b, rect.Left + rect.Width / 2);
-            Canvas.SetTop(_b, rect.Top + rect.Height);
+            Canvas.SetLeft(_bottom, rect.Center.X);
+            Canvas.SetTop(_bottom, rect.Bottom);
 
-            Canvas.SetLeft(_l, rect.Left);
-            Canvas.SetTop(_l, rect.Top + rect.Height / 2);
+            Canvas.SetLeft(_left, rect.Left);
+            Canvas.SetTop(_left, rect.Center.Y);
             
-            Canvas.SetLeft(_r, rect.Left + rect.Width);
-            Canvas.SetTop(_r, rect.Top + rect.Height / 2);
+            Canvas.SetLeft(_right, rect.Right);
+            Canvas.SetTop(_right, rect.Center.Y);
 
-            Canvas.SetLeft(_ul, rect.Left);
-            Canvas.SetTop(_ul, rect.Top);
+            Canvas.SetLeft(_topLeft, rect.Left);
+            Canvas.SetTop(_topLeft, rect.Top);
 
-            Canvas.SetLeft(_ur, rect.Left + rect.Width);
-            Canvas.SetTop(_ur, rect.Top);
+            Canvas.SetLeft(_topRight, rect.Right);
+            Canvas.SetTop(_topRight, rect.Top);
 
-            Canvas.SetLeft(_ll, rect.Left);
-            Canvas.SetTop(_ll, rect.Top + rect.Height);
+            Canvas.SetLeft(_bottomLeft, rect.Left);
+            Canvas.SetTop(_bottomLeft, rect.Bottom);
 
-            Canvas.SetLeft(_lr, rect.Left + rect.Width);
-            Canvas.SetTop(_lr, rect.Top + rect.Height);
-   
-            Canvas.SetLeft(_drag, rect.Left);
-            Canvas.SetTop(_drag, rect.Top);
-            _drag.Width = rect.Width;
-            _drag.Height = rect.Height;
+            Canvas.SetLeft(_bottomRight, rect.Right);
+            Canvas.SetTop(_bottomRight, rect.Bottom);
         }
 
-        private void OnDragDeltaT(object? sender, VectorEventArgs e)
+        private void UpdateDrag(Rect rect)
         {
-            var rect = GetRect();
-            var inflated = rect.Inflate(new Thickness(0, -e.Vector.Y, 0, 0));
-            UpdateThumbs(inflated);
-            UpdateControl(Control, inflated);
+            if (_drag is { })
+            {
+                Canvas.SetLeft(_drag, rect.Left);
+                Canvas.SetTop(_drag, rect.Top);
+
+                _drag.Width = rect.Width;
+                _drag.Height = rect.Height;
+            }
         }
 
-        private void OnDragDeltaB(object? sender, VectorEventArgs e)
+        private void OnDragDeltaDrag(object? sender, VectorEventArgs e)
         {
+            Canvas.SetLeft(_top, Canvas.GetLeft(_top) + e.Vector.X);
+            Canvas.SetLeft(_bottom, Canvas.GetLeft(_bottom) + e.Vector.X);
+
+            Canvas.SetLeft(_left, Canvas.GetLeft(_left) + e.Vector.X);
+            Canvas.SetLeft(_right, Canvas.GetLeft(_right) + e.Vector.X);
+
+            Canvas.SetLeft(_topLeft, Canvas.GetLeft(_topLeft) + e.Vector.X);
+            Canvas.SetLeft(_bottomLeft, Canvas.GetLeft(_bottomLeft) + e.Vector.X);
+
+            Canvas.SetLeft(_topRight, Canvas.GetLeft(_topRight) + e.Vector.X);
+            Canvas.SetLeft(_bottomRight, Canvas.GetLeft(_bottomRight) + e.Vector.X);
+
+            Canvas.SetTop(_top, Canvas.GetTop(_top) + e.Vector.Y);
+            Canvas.SetTop(_bottom, Canvas.GetTop(_bottom) + e.Vector.Y);
+
+            Canvas.SetTop(_left, Canvas.GetTop(_left) + e.Vector.Y);
+            Canvas.SetTop(_right, Canvas.GetTop(_right) + e.Vector.Y);
+
+            Canvas.SetTop(_topLeft, Canvas.GetTop(_topLeft) + e.Vector.Y);
+            Canvas.SetTop(_topRight, Canvas.GetTop(_topRight) + e.Vector.Y);
+
+            Canvas.SetTop(_bottomLeft, Canvas.GetTop(_bottomLeft) + e.Vector.Y);
+            Canvas.SetTop(_bottomRight, Canvas.GetTop(_bottomRight) + e.Vector.Y);
+
             var rect = GetRect();
-            var inflated = rect.Inflate(new Thickness(0, 0, 0, e.Vector.Y));
-            UpdateThumbs(inflated);
-            UpdateControl(Control, inflated);
+
+            UpdateDrag(rect);
+
+            if (Control is { })
+            {
+                UpdateControl(Control, rect);
+            }
         }
 
-        private void OnDragDeltaL(object? sender, VectorEventArgs e)
+        private void OnDragDeltaTop(object? sender, VectorEventArgs e)
         {
+            Canvas.SetTop(_top, Canvas.GetTop(_top) + e.Vector.Y);
+            Canvas.SetTop(_topLeft, Canvas.GetTop(_topLeft) + e.Vector.Y);
+            Canvas.SetTop(_topRight, Canvas.GetTop(_topRight) + e.Vector.Y);
+
             var rect = GetRect();
-            var inflated = rect.Inflate(new Thickness(-e.Vector.X, 0, 0, 0));
-            UpdateThumbs(inflated);
-            UpdateControl(Control, inflated);
+
+            Canvas.SetTop(_left, rect.Center.Y);
+            Canvas.SetTop(_right, rect.Center.Y);
+  
+            UpdateDrag(rect);
+
+            if (Control is { })
+            {
+                UpdateControl(Control, rect);
+            }
         }
 
-        private void OnDragDeltaR(object? sender, VectorEventArgs e)
+        private void OnDragDeltaBottom(object? sender, VectorEventArgs e)
         {
+            Canvas.SetTop(_bottom, Canvas.GetTop(_bottom) + e.Vector.Y);
+            Canvas.SetTop(_bottomLeft, Canvas.GetTop(_bottomLeft) + e.Vector.Y);
+            Canvas.SetTop(_bottomRight, Canvas.GetTop(_bottomRight) + e.Vector.Y);
+
             var rect = GetRect();
-            var inflated = rect.Inflate(new Thickness(0, 0, e.Vector.X, 0));
-            UpdateThumbs(inflated);
-            UpdateControl(Control, inflated);
+
+            Canvas.SetTop(_left, rect.Center.Y);
+            Canvas.SetTop(_right, rect.Center.Y);
+
+            UpdateDrag(rect);
+
+            if (Control is { })
+            {
+                UpdateControl(Control, rect);
+            }
+        }
+
+        private void OnDragDeltaLeft(object? sender, VectorEventArgs e)
+        {
+            Canvas.SetLeft(_left, Canvas.GetLeft(_left) + e.Vector.X);
+            Canvas.SetLeft(_topLeft, Canvas.GetLeft(_topLeft) + e.Vector.X);
+            Canvas.SetLeft(_bottomLeft, Canvas.GetLeft(_bottomLeft) + e.Vector.X);
+
+            var rect = GetRect();
+
+            Canvas.SetLeft(_top, rect.Center.X);
+            Canvas.SetLeft(_bottom, rect.Center.X);
+
+            UpdateDrag(rect);
+
+            if (Control is { })
+            {
+                UpdateControl(Control, rect);
+            }
+        }
+
+        private void OnDragDeltaRight(object? sender, VectorEventArgs e)
+        {
+            Canvas.SetLeft(_right, Canvas.GetLeft(_right) + e.Vector.X);
+            Canvas.SetLeft(_topRight, Canvas.GetLeft(_topRight) + e.Vector.X);
+            Canvas.SetLeft(_bottomRight, Canvas.GetLeft(_bottomRight) + e.Vector.X);
+
+            var rect = GetRect();
+
+            Canvas.SetLeft(_top, rect.Center.X);
+            Canvas.SetLeft(_bottom, rect.Center.X);
+
+            UpdateDrag(rect);
+
+            if (Control is { })
+            {
+                UpdateControl(Control, rect);
+            }
         }
  
-        private void OnDragDeltaUL(object? sender, VectorEventArgs e)
+        private void OnDragDeltaTopLeft(object? sender, VectorEventArgs e)
         {
+            Canvas.SetLeft(_left, Canvas.GetLeft(_left) + e.Vector.X);
+            Canvas.SetLeft(_topLeft, Canvas.GetLeft(_topLeft) + e.Vector.X);
+            Canvas.SetLeft(_bottomLeft, Canvas.GetLeft(_bottomLeft) + e.Vector.X);
+
+            Canvas.SetTop(_top, Canvas.GetTop(_top) + e.Vector.Y);
+            Canvas.SetTop(_topLeft, Canvas.GetTop(_topLeft) + e.Vector.Y);
+            Canvas.SetTop(_topRight, Canvas.GetTop(_topRight) + e.Vector.Y);
+
             var rect = GetRect();
-            var inflated = rect.Inflate(new Thickness(-e.Vector.X, -e.Vector.Y, 0, 0));
-            UpdateThumbs(inflated);
-            UpdateControl(Control, inflated);
+
+            Canvas.SetLeft(_top, rect.Center.X);
+            Canvas.SetLeft(_bottom, rect.Center.X);
+
+            Canvas.SetTop(_left, rect.Center.Y);
+            Canvas.SetTop(_right, rect.Center.Y);
+
+            UpdateDrag(rect);
+
+            if (Control is { })
+            {
+                UpdateControl(Control, rect);
+            }
         }
 
-        private void OnDragDeltaUR(object? sender, VectorEventArgs e)
+        private void OnDragDeltaTopRight(object? sender, VectorEventArgs e)
         {
+            Canvas.SetLeft(_right, Canvas.GetLeft(_right) + e.Vector.X);
+            Canvas.SetLeft(_topRight, Canvas.GetLeft(_topRight) + e.Vector.X);
+            Canvas.SetLeft(_bottomRight, Canvas.GetLeft(_bottomRight) + e.Vector.X);
+
+            Canvas.SetTop(_top, Canvas.GetTop(_top) + e.Vector.Y);
+            Canvas.SetTop(_topLeft, Canvas.GetTop(_topLeft) + e.Vector.Y);
+            Canvas.SetTop(_topRight, Canvas.GetTop(_topRight) + e.Vector.Y);
+            
             var rect = GetRect();
-            var inflated = rect.Inflate(new Thickness(0, -e.Vector.Y, e.Vector.X, 0));
-            UpdateThumbs(inflated);
-            UpdateControl(Control, inflated);
+
+            Canvas.SetLeft(_top, rect.Center.X);
+            Canvas.SetLeft(_bottom, rect.Center.X);
+
+            Canvas.SetTop(_left, rect.Center.Y);
+            Canvas.SetTop(_right, rect.Center.Y);
+
+            UpdateDrag(rect);
+
+            if (Control is { })
+            {
+                UpdateControl(Control, rect);
+            }
         }
 
-        private void OnDragDeltaLL(object? sender, VectorEventArgs e)
+        private void OnDragDeltaBottomLeft(object? sender, VectorEventArgs e)
         {
+            Canvas.SetLeft(_left, Canvas.GetLeft(_left) + e.Vector.X);
+            Canvas.SetLeft(_topLeft, Canvas.GetLeft(_topLeft) + e.Vector.X);
+            Canvas.SetLeft(_bottomLeft, Canvas.GetLeft(_bottomLeft) + e.Vector.X);
+
+            Canvas.SetTop(_bottom, Canvas.GetTop(_bottom) + e.Vector.Y);
+            Canvas.SetTop(_bottomLeft, Canvas.GetTop(_bottomLeft) + e.Vector.Y);
+            Canvas.SetTop(_bottomRight, Canvas.GetTop(_bottomRight) + e.Vector.Y);
+
             var rect = GetRect();
-            var inflated = rect.Inflate(new Thickness(-e.Vector.X, 0, 0, e.Vector.Y));
-            UpdateThumbs(inflated);
-            UpdateControl(Control, inflated);
+
+            Canvas.SetLeft(_top, rect.Center.X);
+            Canvas.SetLeft(_bottom, rect.Center.X);
+
+            Canvas.SetTop(_left, rect.Center.Y);
+            Canvas.SetTop(_right, rect.Center.Y);
+
+            UpdateDrag(rect);
+
+            if (Control is { })
+            {
+                UpdateControl(Control, rect);
+            }
         }
 
-        private void OnDragDeltaLR(object? sender, VectorEventArgs e)
+        private void OnDragDeltaBottomRight(object? sender, VectorEventArgs e)
         {
-            var rect = GetRect();
-            var inflated = rect.Inflate(new Thickness(0, 0, e.Vector.X, e.Vector.Y));
-            UpdateThumbs(inflated);
-            UpdateControl(Control, inflated);
-        }
+            Canvas.SetLeft(_right, Canvas.GetLeft(_right) + e.Vector.X);
+            Canvas.SetLeft(_topRight, Canvas.GetLeft(_topRight) + e.Vector.X);
+            Canvas.SetLeft(_bottomRight, Canvas.GetLeft(_bottomRight) + e.Vector.X);
 
-        private void OnDragDeltaDRAG(object? sender, VectorEventArgs e)
-        {
-            var left = Canvas.GetLeft(_drag) + e.Vector.X;
-            var top = Canvas.GetTop(_drag) + e.Vector.Y;
-            Canvas.SetLeft(_drag, left);
-            Canvas.SetTop(_drag, top);
-            var rect = new Rect(left, top, _drag.Width, _drag.Height);
-            UpdateThumbs(rect);
-            UpdateControl(Control, rect);
+            Canvas.SetTop(_bottom, Canvas.GetTop(_bottom) + e.Vector.Y);
+            Canvas.SetTop(_bottomLeft, Canvas.GetTop(_bottomLeft) + e.Vector.Y);
+            Canvas.SetTop(_bottomRight, Canvas.GetTop(_bottomRight) + e.Vector.Y);
+
+            var rect = GetRect();
+
+            Canvas.SetLeft(_top, rect.Center.X);
+            Canvas.SetLeft(_bottom, rect.Center.X);
+
+            Canvas.SetTop(_left, rect.Center.Y);
+            Canvas.SetTop(_right, rect.Center.Y);
+
+            UpdateDrag(rect);
+
+            if (Control is { })
+            {
+                UpdateControl(Control, rect);
+            }
         }
     }
 }
